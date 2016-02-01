a812484a99f0fd33593ce6afe3071b488e6fde66
Armand0s <armand.souchon@gmail.com>
2016-02-01 13:04:01 +0100

    creation

---
+++ class.py
@@ -0,0 +1,62 @@
+class car():
+    def __init__(self,body,shirt,head):
+        self.body=body
+        self.shirt=shirt
+        self.head=head
+
+    def draw(screen,x,y):
+        # a faire
+
+class cycle():
+    def __init__(self,screen,pants,shirt,head,cycle):
+        self.body=body
+        self.pants=pants
+        self.head=head
+        self.cycle=cycle
+
+    def draw(screen,x,y):
+        # a faire
+
+class piedestrian():
+    def __init__(self,screen,pants,shirt,head):
+        self.pants=pants
+        self.shirt=shirt
+        self.head=head
+
+    def draw(screen,x,y):
+        # a faire
+
+
+class retro():
+    def __init__(self,screen,x,y):
+        self.x=x
+        self.y=y
+
+    def draw(screen):
+        # a faire
+
+class light():
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+
+    def draw(screen):
+        pygame.circle(screen,(216,248,120),(x,y),10,0)
+        # done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
